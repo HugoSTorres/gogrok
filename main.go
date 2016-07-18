e05afa8e92ee8d2112feb5cd90d015300eebbb39
Hugo Torres <hugo.torres1993@gmail.com>
2016-07-18 08:26:22 -0400

    Handle interrupt signals more cleanly

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 )
 
 func main() {
@@ -14,6 +16,20 @@ func main() {
 	}
 
 	ch := make(chan Message)
+
+	// Handle signals cleanly
+	sig := make(chan os.Signal, 1)
+	exit := make(chan struct{})
+	signal.Notify(sig, os.Interrupt)
+
+	go func() {
+		for {
+			select {
+			case <-sig:
+				exit <- struct{}{}
+			}
+		}
+	}()
 
 	go func() {
 		err = sess.Record(ch)
@@ -30,5 +46,19 @@ func main() {
 		return
 	})
 
-	log.Fatalf("error serving http: %s\n", http.ListenAndServe(":3000", nil))
+	// Let's give the server three tries to start up, just in case something rare
+	// and intermittent happens.
+	go func() {
+		for i := 0; i < 3; i++ {
+			log.Fatalf("error serving http: %s\n", http.ListenAndServe(":3000", nil))
+		}
+	}()
+
+	<-exit
+
+	log.Println("shutting down")
+
+	close(exit)
+	close(sig)
+	close(ch)
 }
